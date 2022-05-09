7ddc65d90d0f39ea2f7c506c2c8ec3f3d4f49ca3
Bui-Anh-Vy <95459666+Bui-Anh-Vy@users.noreply.github.com>
2022-05-09 15:55:00 +0700

    Create footer.js

---
+++ footer.js
@@ -0,0 +1,12 @@
+const footer = `
+    <nav>
+        <div class="footer">
+            <div class="code1">
+                <a href="#">Help links</a>
+                <a href="#">About</a>
+                <a href="#">Privacy</a>
+                <a href="#">Copyright</a>
+            </div>
+        </div>
+    </nav>
+`
