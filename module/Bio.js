8ffbcee4a24d009e01496bc2557183eaebce68b3
Bui-Anh-Vy <95459666+Bui-Anh-Vy@users.noreply.github.com>
2022-05-12 11:33:14 +0700

    Create Bio.js

---
+++ module/Bio.js
@@ -0,0 +1,15 @@
+const Bio = () =>
+    `
+    <section class="bio">
+        <div class="profile-photo">
+            <i class="add-image"></i>
+            <img src="./z3391548739229_e9fa39df14c9b1bf726444d143d56479.jpg" alt="profile-photo">
+        </div>
+        <div class="profile-info">
+            <p class="username">buia4869</p>
+            <button>Edit profile</button>
+        </div>
+    </section>
+`
+
+export default Bio;
