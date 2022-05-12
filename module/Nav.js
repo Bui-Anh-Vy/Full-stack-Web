9e368e4f9e9032032a02948b1abf82681fcba1bf
Bui-Anh-Vy <95459666+Bui-Anh-Vy@users.noreply.github.com>
2022-05-12 11:34:05 +0700

    Add files via upload

---
+++ module/Nav.js
@@ -0,0 +1,17 @@
+const Nav = () =>
+`
+    <nav>
+        <a href=# class=logo>
+            <img src="./image.jpg" alt="logo">
+            <a href="http://127.0.0.1:5500/#"></a>
+        </a>
+        <input type="text" class="search" placeholder="search">
+        <span class="nav-links">
+            <a href="#"><i class="fa-solid fa-house"></i></a>
+            <a href='#'><i class="fa-regular fa-user"></i></a>
+        </span>
+    </nav>
+`
+export default Nav;
+
+
