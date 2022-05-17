35132e4cc5d353371b98c06db7c2d6811c3c7fbc
khoirmit2402 <80302862+khoirmit2402@users.noreply.github.com>
2022-05-17 12:00:36 +0700

    Add files via upload

---
+++ footer/about.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title> Help page </title>
+    <link rel="stylesheet" href="/footer/about.css">
+</head>
+
+<body>
+    <?php include('../modules/navbar.php'); ?>
+    <section>
+        <h2 style="text-align:center">Our Team</h2>
+        <div class="row">
+            <div class="column">
+                <div class="card">
+                    <img src="./download(1).jpg" alt="Vy" style="width: 100%">
+                    <div class="container">
+                        <h2>Bui Anh Vy</h2>
+                        <p class="title">UI </p>
+                        <p>text</p>
+                        <p>s3891641@rmit.edu.vn</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="column">
+                <div class="card">
+                    <img src="./download(3).jpg" alt="Phaxt" style="width: 100%">
+                    <div class="container">
+                        <h2>Phaxt Tran</h2>
+                        <p class="title">Cookies </p>
+                        <p>text</p>
+                        <p>s*******@rmit.edu.vn</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="column">
+                <div class="card1">
+                    <img src="./download.jpg" alt="Kha" style="width: 100%">
+                    <div class="container">
+                        <h2>Kha Vo Nguyen Nhat</h2>
+                        <p class="title">Images </p>
+                        <p>text</p>
+                        <p>s******@rmit.edu.vn</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="column">
+                <div class="card">
+                    <img src="./download(2).jpg" alt="Kian" style="width: 100%">
+                    <div class="container">
+                        <h2>Kian Nguyen</h2>
+                        <p class="title">Logged in/out </p>
+                        <p>text</p>
+                        <p>s*******@rmit.edu.vn</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <?php include('../modules/footer.php'); ?>
+
+</body>
+
+</html>
---
+++ footer/help.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title> Help page </title>
+    <link rel="stylesheet" href="/footer/help.css">
+</head>
+<body>
+<?php include('../modules/navbar.php'); ?>
+
+        <div class="about-section">
+            <h1>How can we help you?</h1>
+            <input type="text" class="search" placeholder="Search help articles">
+        </div>
+        <div class="question">
+            <h1>Frequently asked questions</h1>
+            <p>What are the requirements for an InstaKilogram account?</p>
+            <p>How to delete promoted Instagram posts?</p>
+        </div>
+
+    <?php include('../modules/footer.php'); ?>
+
+</body>
+</html>
