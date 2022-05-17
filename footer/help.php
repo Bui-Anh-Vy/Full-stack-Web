783a6148d8751efc1b3311029b2a0558b3ff82d3
khoirmit2402 <80302862+khoirmit2402@users.noreply.github.com>
2022-05-17 20:53:29 +0700

    Update help.php

--- footer/help.php
+++ footer/help.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title> Help page </title>
-    <link rel="stylesheet" href="/footer/help.css">
+    <link rel="stylesheet" href="/style.css">
 </head>
 <body>
 <?php include('../modules/navbar.php'); ?>
